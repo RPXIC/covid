e14d95e1f990fe7bca303ba33d5181ec8dc729c2
RPXIC <rpc2works@gmail.com>
2020-09-09 19:53:37 +0200

    first global retrieve

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState()
+  const [global, setGlobal] = useState()
+  const [date, setDate] = useState()
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const response = await res.json()
+      const { Global, Countries, Date } = response
+      setGlobal(Global)
+      setCountries(Countries)
+      setDate(Date)
+      console.log(response)
+    })()
+  }, [])
+
+  if (!date || !global || !countries) return <div className="App">loading...</div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Latest update: {date}</div>
+      <div>New confirmed: {global.NewConfirmed}</div>
+      <div>New deaths: {global.NewDeaths}</div>
+      <div>New recovered: {global.NewRecovered}</div>
+      <div>Total confirmed: {global.TotalConfirmed}</div>
+      <div>Total Deaths: {global.TotalDeaths}</div>
+      <div>Total recovered: {global.TotalRecovered}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
