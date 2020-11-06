7c88de0ae719acce5b7d7f1f0182e1c4f9e3de03
RPXIC <rpc2works@gmail.com>
2020-11-06 15:18:45 +0100

    chart styles

--- src/pages/country.js
+++ src/pages/country.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Loader } from 'components'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { Bar } from 'react-chartjs-2'
 import useGetCountryConfirmedLastMonth from 'hooks/useGetConfirmedLastMonth'
+import handleChartData from 'utils/handleChartData'
+import './country.sass'
 
 const Country = () => {
     const { Country } = useParams()
@@ -10,37 +12,14 @@ const Country = () => {
 
     if (loading) return <Loader />
 
-    let actives = []
-    let days = []
-
-    if (chartData) {
-        chartData.forEach(day => {
-            actives.push(day.Active)
-            days.push(day.Date)
-        })
-    }
-
-    const data = {
-        labels: days,
-        datasets: [{
-            label: `Active cases in ${Country}`,
-            backgroundColor: 'rgba(0,255,0,1)',
-            borderColor: 'black',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(0,255,0,0.2)',
-            hoverBorderColor: '#FFFF00',
-            data: actives
-        }]
-    }
-
-    const options = {
-        maintainAspectRatio: false,
-        responsive: true
-    }
+    const { data, options } = handleChartData(chartData, Country)
 
     return (
-        <div style={{width: '90%', height: '500px'}}>
-            <Bar data={data} options={options} />
+        <div className="chart">
+            <Link to="/" className="chart__link">Go back</Link>
+            <div className="chart__content">
+                <Bar data={data} options={options} />
+            </div>
         </div>
     )
 }
