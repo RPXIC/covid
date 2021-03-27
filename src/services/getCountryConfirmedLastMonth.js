7e65b75e8e62f90965466b906ece046578308a8b
rpxic <rpc2works@gmail.com>
2021-03-27 11:49:32 +0100

    ok

--- src/services/getCountryConfirmedLastMonth.js
+++ src/services/getCountryConfirmedLastMonth.js
@@ -4,7 +4,6 @@ const getCountryConfirmedLastMonth = async (country) => {
 	const year = date.getFullYear()
 	const fromMonth = month === 0 ? 11 : month
 	const fromYear = month === 0 ? date.getFullYear() - 1 : date.getFullYear()
-	console.log(fromMonth)
 	const res = await fetch(
 		`https://api.covid19api.com/country/${country}?from=${fromYear}-${fromMonth}-01T00:00:00Z&to=${year}-${month}-01T00:00:00Z`
 	)
