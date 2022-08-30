900fe02ba5e7efa3b1b61ffad9d2f6f277879859
rpxic <rpc2works@gmail.com>
2022-08-30 13:30:17 +0200

    fix date parser

--- src/utils/dateParser.js
+++ src/utils/dateParser.js
@@ -1,8 +1,8 @@
 const dateParser = (date) => {
   const day = new Date(date).getDate()
-  const month = new Date(date).getMonth()
+  const month = new Date(date).getMonth() + 1
   const year = new Date(date).getFullYear()
-  const hours = new Date(date).getHours()
+  const hours = new Date(date).getUTCHours()
   const minutes = new Date(date).getMinutes()
 
   const parsedDate = `${day}/${month}/${year} ${hours}:${minutes}h.`
